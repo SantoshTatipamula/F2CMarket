2dcec3682f0c6e4de16b6ef0b96b14d91bca3206
Santosh Tatipamula <santoshtatipamula520@gmail.com>
2026-06-28 15:20:43 +0530

    Implement Firebase Google authentication service

--- src/services/firebaseAuth.js
+++ src/services/firebaseAuth.js
@@ -3,15 +3,21 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 
 import { auth } from "@/config/firebase";
+
+const googleProvider = new GoogleAuthProvider();
 
 export const registerWithEmail = (email, password) =>
   createUserWithEmailAndPassword(auth, email, password);
 
 export const loginWithEmail = (email, password) =>
   signInWithEmailAndPassword(auth, email, password);
+
+export const loginWithGoogle = () => signInWithPopup(auth, googleProvider);
 
 export const logoutUser = () => signOut(auth);
 
